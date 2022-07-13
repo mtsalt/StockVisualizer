a69957b93cdeb96e50ad356014e8026725444e91
mtsalt <juntaro.shioyama@gmail.com>
2022-07-13 23:19:49 +0900

    add chart with chart.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
-var chart = c3.generate({
-    bindto: '#stock-chart',
-    size: { width: 1800, height: 500},
-    data: {
-      columns: [
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]
-    }
-});
+// var chart = c3.generate({
+//     bindto: '#stock-chart',
+//     size: { width: 1800, height: 500},
+//     data: {
+//       columns: [
+//         ['data1', 30, 200, 100, 400, 150, 250],
+//         ['data2', 50, 20, 10, 40, 15, 25]
+//       ]
+//     }
+// });
